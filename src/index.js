e51b13ffc0136e4deca0a928db2930fc23dc145b
anuragjatofficial <anurag.1101140@gmail.com>
2023-06-01 23:31:15 +0530

    fixed smooth scroll

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ document.getElementById("downloadCV").addEventListener("click",()=>{
     window.open('https://drive.google.com/u/0/uc?id=1vZeu-xiBHf0hE66oxgVPPHuOovePhYfr&export=download');
 });
 
-document.getElementById("resume-link-1").addEventListener("click", () => {
+document.getElementById("resume-button-1").addEventListener("click", () => {
     window.open('https://drive.google.com/u/0/uc?id=1vZeu-xiBHf0hE66oxgVPPHuOovePhYfr&export=download');
+});
+
+//for smooth scrolling
+document.querySelectorAll('#nav-menu>ul>li>a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener('click',function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
 });
