c85f617c56060f768683c1c8e9574fa0adf8309d
anuragjatofficial <anurag.1101140@gmail.com>
2023-06-01 22:57:31 +0530

    fixed download resume part

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.getElementById("downloadCV").addEventListener("click",()=>{
-    
-})
+    window.open('https://drive.google.com/u/0/uc?id=1vZeu-xiBHf0hE66oxgVPPHuOovePhYfr&export=download');
+});
+
+document.getElementById("resume-link-1").addEventListener("click", () => {
+    window.open('https://drive.google.com/u/0/uc?id=1vZeu-xiBHf0hE66oxgVPPHuOovePhYfr&export=download');
+});
