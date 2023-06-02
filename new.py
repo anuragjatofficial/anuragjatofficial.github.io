85b21d27d7a0bb2f314c724d3b7a280b8786faa5
anuragjatofficial <anurag.1101140@gmail.com>
2023-06-03 00:14:07 +0530

    tried fixing download

---
+++ new.py
@@ -0,0 +1,9 @@
+#by anurag
+import pyautogui as pg
+import time
+time.sleep(8)
+for i in range(500):
+    pg.write("bosdadiwale")
+    pg.write("behan ke lode")
+    pg.write("betichod")
+    pg.press('enter')
