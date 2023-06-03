82a238a3d8352f3370edda74114fd76446454938
anuragjatofficial <anurag.1101140@gmail.com>
2023-06-03 16:49:09 +0530

    added github stats

--- src/index.js
+++ src/index.js
@@ -22,7 +22,8 @@ document.getElementById("resume-button-1").addEventListener("click", () => {
     window.open('https://drive.google.com/u/0/uc?id=1vZeu-xiBHf0hE66oxgVPPHuOovePhYfr&export=download');
 });*/
 
-//for smooth scrolling
+//---------------- for smooth scrolling ----------------//
+
 document.querySelectorAll('#nav-menu>ul>li>a[href^="#"]').forEach(anchor =>{
     anchor.addEventListener('click',function(e){
         e.preventDefault();
@@ -31,3 +32,7 @@ document.querySelectorAll('#nav-menu>ul>li>a[href^="#"]').forEach(anchor =>{
         });
     });
 });
+
+
+GitHubCalendar(".react-activity-calendar", "your-username");
+GitHubCalendar(".react-activity-calendar", "anuragjatofficial", { responsive: true });
