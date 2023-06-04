54c656b68cad4456ac18fca319c0047bc8b8f8e3
anuragjatofficial <anurag.1101140@gmail.com>
2023-06-04 21:04:01 +0530

    final version for desktop

--- new.py
+++ new.py
@@ -3,7 +3,7 @@ import pyautogui as pg
 import time
 time.sleep(8)
 for i in range(500):
-    pg.write("bosdadiwale")
-    pg.write("behan ke lode")
-    pg.write("betichod")
+    pg.write("nothing")
+    pg.write("nothing")
+    pg.write("nothing")
     pg.press('enter')
