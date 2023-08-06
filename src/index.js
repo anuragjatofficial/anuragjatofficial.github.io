3bba5459cf8f239b24b655608e2d4c23051a36b4
anuragjatofficial <anurag.1101140@gmail.com>
2023-08-07 01:26:40 +0530

    added 2 more projects

--- src/index.js
+++ src/index.js
@@ -103,6 +103,22 @@ let projects_array = [
         tech_stacks: "Java | JPA Hibernate | Mysql",
         github_link: "https://github.com/anuragjatofficial/major-flock-5344.git",
         deployed_link: "https://drive.google.com/file/d/10npOdmOa27-mL__n7wuaFJflw5aW3nGy/view?usp=sharing",
+    },
+    {
+        screenshots: ["./images/covisafe.png", "", "", "", ""],
+        project_title: "Covisafe+ : Your Trusted COVID Vaccine Companion",
+        project_desc: "Covisafe+  offers real-time information, risk assessment, vaccination booking, and community support to combat the pandemic effectively.",
+        tech_stacks: "Java | Spring Boot | Mysql | Sprnig Data JPA | Maven | HTML | CSS | JS",
+        github_link: "https://github.com/anuragjatofficial/potent-voyage-8716",
+        deployed_link: "https://drive.google.com/file/d/10npOdmOa27-mL__n7wuaFJflw5aW3nGy/view?usp=sharing",
+    },
+    {
+        screenshots: ["./images/codepulse2.png", "", "", "", ""],
+        project_title: "CodePulse : Amplifying Your Coding Potential ",
+        project_desc: "An AI interview platform to practice with AI-driven simulations tailored to your skills and industry. Get instant feedback and performance insights to refine your approach.",
+        tech_stacks: "Java | Spring Boot | Mysql | Sprnig Data JPA | Maven | React | TypeScript | CSS ",
+        github_link: "https://github.com/anuragjatofficial/code-pulse.git",
+        deployed_link: "https://codepulse.me/interview_Master",
     }
 ];
 
@@ -128,6 +144,7 @@ function createProjects(project) {
 }
 
 // ------------------------logic for window replace ---------- ///
+
 function resume() {
     window.open("https://drive.google.com/file/d/1vZeu-xiBHf0hE66oxgVPPHuOovePhYfr/view?usp=sharing");
 }
@@ -135,7 +152,7 @@ function resume() {
 
 let originalOverflow = document.body.style.overflow;
 const overlay = document.createElement("div");
-document.querySelectorAll('.imgDiv').forEach((e)=>{
+document.querySelectorAll('.imgDiv').forEach((e,i)=>{
     
     e.addEventListener('click', (event) => {
         event.stopPropagation();
@@ -156,7 +173,7 @@ document.querySelectorAll('.imgDiv').forEach((e)=>{
         overlay.style.height = "100%";
         // overlay.style.zIndex = 1;
         document.querySelector('header').style.zIndex = 0;
-        // document.querySelector(".imgDiv>")
+        document.querySelectorAll(".imgDiv>img")[i].style.borderRadius = "0px";
         document.querySelector('#back-to-top').style.zIndex = 0;
         overlay.style.backgroundColor = "rgba(3, 3, 3, 0.7)"; // White color with 70% opacity
         overlay.style.backdropFilter = "blur(15px)"; // Apply blur to the entire overlay
@@ -168,7 +185,7 @@ document.querySelectorAll('.imgDiv').forEach((e)=>{
 })
 
 document.addEventListener("click", function (event) {
-    document.querySelectorAll('.imgDiv').forEach((e) => {
+    document.querySelectorAll('.imgDiv').forEach((e,i) => {
         e.style.position = ""; // Reset the position
         e.style.zIndex = ""; // Reset the z-index
         e.style.transform = ""; // Reset the transform
@@ -180,6 +197,7 @@ document.addEventListener("click", function (event) {
         overlay.style.display = "none";
         document.body.style.pointerEvents = "auto";
         document.querySelector('header').style.zIndex = 1;
+        document.querySelectorAll(".imgDiv>img")[i].style.borderRadius = "";
         document.querySelector('#back-to-top').style.zIndex = 1;
     });
 });
