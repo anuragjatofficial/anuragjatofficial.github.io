cd5e7ed72eafabd7d73921a42ee93b0eed31d3fe
anuragjatofficial <anurag.1101140@gmail.com>
2023-08-07 23:59:50 +0530

    fixed eductation color on hover

--- src/index.js
+++ src/index.js
@@ -221,4 +221,4 @@ document.onkeydown = function (e) {
      return 123 != event.keyCode && (
         (!e.ctrlKey || !e.shiftKey || e.keyCode != "C".charCodeAt(0)) && ((!e.ctrlKey || !e.shiftKey || e.keyCode != "J".charCodeAt(0)) && ((!e.ctrlKey || e.keyCode != "U".charCodeAt(0)) && void 0))
         )
-}
+} 
