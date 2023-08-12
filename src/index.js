6def7467eca6d0276632694bab3ed008bd5146d3
anuragjatofficial <anurag.1101140@gmail.com>
2023-08-12 12:07:36 +0530

    fixed some bugs

--- src/index.js
+++ src/index.js
@@ -17,14 +17,6 @@ var typed = new Typed(".typing2", {
     loop: true,
 });
 
-/*document.getElementById("downloadCV").addEventListener("click",()=>{
-    window.open('https://drive.google.com/u/0/uc?id=1vZeu-xiBHf0hE66oxgVPPHuOovePhYfr&export=download');
-});
-
-document.getElementById("resume-button-1").addEventListener("click", () => {
-    window.open('https://drive.google.com/u/0/uc?id=1vZeu-xiBHf0hE66oxgVPPHuOovePhYfr&export=download');
-});*/
-
 //---------------- for smooth scrolling ----------------//
 
 // document.querySelectorAll('#nav-menu a[href^="#"]').forEach(anchor =>{
@@ -72,11 +64,16 @@ function CreateGitHubCalendar(){
 
 // ------------logic for navBar ---------------------//
 
-document.querySelectorAll(".odd-nav").forEach(a => {
-    a.addEventListener("click", (e) => {
-        document.querySelector("header").style.backgroundColor = "var(--third-color)";
-    })
-});
+document
+    .querySelectorAll(".odd-nav")
+    .forEach(a => {
+        a.addEventListener("click", (e) => {
+            document
+                .querySelector("header")
+                .style
+                .backgroundColor = "var(--third-color)";
+        })
+    });
 
 
 // --------- for dynamically adding projects -------///
@@ -136,20 +133,35 @@ let projects_array = [
 
 projects_array.forEach(e => {
     let t = createProjects(e);
-    projects.append(t);
+    projects
+        .append(t);
 })
 
 function createProjects(project) {
     let div = document.createElement("div");
     div.setAttribute("class", "project-card");
-    div.innerHTML = `<div class="project-box">
-                    <div class="imgDiv"><img src="${project.screenshots[0]}" alt=""></div>
-                    <h2 class="project-title">${project.project_title}</h2>
-                    <p class="project-description">${project.project_desc}</p>
-                    <p class="project-tech-stack"><span>Tech-stack : </span>${project.tech_stacks}</p>
+    div.innerHTML = 
+                `<div class="project-box">
+                    <div class="imgDiv">
+                        <img src="${project.screenshots[0]}" alt="">
+                    </div>
+                    <h2 class="project-title">
+                        ${project.project_title}
+                    </h2>
+                    <p class="project-description">
+                        ${project.project_desc}
+                    </p>
+                    <p class="project-tech-stack">
+                        <span>Tech-stack : </span>
+                        ${project.tech_stacks}
+                    </p>
                     <div>
-                        <a href="${project.github_link}" class="project-github-link" target="_blank"><img src="./images/github.svg" alt=""></a>
-                        <a href="${project.deployed_link}" class="project-deployed-link" target="_blank"><img src="https://img.icons8.com/ios-glyphs/90/visible--v1.png" alt="visible--v1"/></a>
+                        <a href="${project.github_link}" class="project-github-link" target="_blank">
+                            <img src="./images/github.svg" alt="">
+                        </a>
+                        <a href="${project.deployed_link}" class="project-deployed-link" target="_blank">
+                            <img src="https://img.icons8.com/ios-glyphs/90/visible--v1.png" alt="visible--v1"/>
+                        </a>
                     </div>
                 </div>`;
     return div;
@@ -161,6 +173,7 @@ function resume() {
     window.open("https://drive.google.com/file/d/1vZeu-xiBHf0hE66oxgVPPHuOovePhYfr/view?usp=sharing");
 }
 
+// logic to blur the background & zoom project screenshots on click 
 
 let originalOverflow = document.body.style.overflow;
 const overlay = document.createElement("div");
@@ -175,50 +188,110 @@ document.querySelectorAll('.imgDiv').forEach((e,i)=>{
         e.style.left = "50%";
         e.style.width = "80vw";
         e.style.border = "none";
-        document.body.style.pointerEvents = "none";
-        // e.style.width = "100vw";
-        overlay.style.display = "flex";
-        overlay.style.position = "fixed";
+
+        document
+            .body
+            .style
+            .pointerEvents = "none";
+
+        overlay
+            .style
+            .display = "flex";
+
+        overlay
+            .style
+            .position = "fixed";
+            
         overlay.style.top = "0";
         overlay.style.left = "0";
         overlay.style.width = "100%";
         overlay.style.height = "100%";
-        // overlay.style.zIndex = 1;
-        document.querySelector('header').style.zIndex = 0;
-        document.querySelectorAll(".imgDiv>img")[i].style.borderRadius = "0px";
-        document.querySelector('#back-to-top').style.zIndex = 0;
-        overlay.style.backgroundColor = "rgba(3, 3, 3, 0.7)"; // White color with 70% opacity
-        overlay.style.backdropFilter = "blur(15px)"; // Apply blur to the entire overlay
-        document.body.style.overflow = "hidden";
-        document.body.append(overlay);
-    
-        // e.style.filter = "blur(0px)";
+
+        document
+            .querySelector('header')
+            .style
+            .zIndex = 0;
+
+        document
+            .querySelectorAll(".imgDiv>img")[i]
+            .style.borderRadius = "0px";
+
+        document
+            .querySelector('#back-to-top')
+            .style
+            .zIndex = 0;
+
+        overlay
+            .style
+            .backgroundColor = "rgba(3, 3, 3, 0.7)"; // White color with 70% opacity
+
+        overlay
+            .style
+            .backdropFilter = "blur(15px)"; // Apply blur to the entire overlay
+
+        document
+            .body
+            .style
+            .overflow = "hidden";
+
+        document
+            .body
+            .append(overlay);
     })
 })
 
-document.addEventListener("click", function (event) {
-    document.querySelectorAll('.imgDiv').forEach((e,i) => {
-        e.style.position = ""; // Reset the position
-        e.style.zIndex = ""; // Reset the z-index
-        e.style.transform = ""; // Reset the transform
-        e.style.top = ""; // Reset the top
-        e.style.left = ""; // Reset the left
-        e.style.width = ""; // Reset the width
-        document.querySelector("body").style.filter = "";
-        document.body.style.overflow = originalOverflow;
-        overlay.style.display = "none";
-        document.body.style.pointerEvents = "auto";
-        document.querySelector('header').style.zIndex = 1;
-        document.querySelectorAll(".imgDiv>img")[i].style.borderRadius = "";
-        document.querySelector('#back-to-top').style.zIndex = 1;
+document
+    .addEventListener("click", function (event) {
+        document
+            .querySelectorAll('.imgDiv').forEach((e,i) => {
+                e.style.position = ""; // Reset the position
+                e.style.zIndex = ""; // Reset the z-index
+                e.style.transform = ""; // Reset the transform
+                e.style.top = ""; // Reset the top
+                e.style.left = ""; // Reset the left
+                e.style.width = ""; // Reset the width
+
+                document
+                    .querySelector("body")
+                    .style
+                    .filter = "";
+
+                document
+                    .body
+                    .style
+                    .overflow = originalOverflow;
+
+                overlay
+                    .style
+                    .display = "none";
+
+                document
+                    .body
+                    .style
+                    .pointerEvents = "auto";
+
+                document
+                    .querySelector('header')
+                    .style
+                    .zIndex = 1;
+                document
+                    .querySelectorAll(".imgDiv>img")[i]
+                    .style
+                    .borderRadius = "";
+                document
+                    .querySelector('#back-to-top')
+                    .style
+                    .zIndex = 1;
+            });
     });
-});
 
 // -- code to disable right click , view source , inspect -- //
 
-document.addEventListener("contextmenu", ( e => e.preventDefault())), 
-document.onkeydown = function (e) {
-     return 123 != event.keyCode && (
-        (!e.ctrlKey || !e.shiftKey || e.keyCode != "C".charCodeAt(0)) && ((!e.ctrlKey || !e.shiftKey || e.keyCode != "J".charCodeAt(0)) && ((!e.ctrlKey || e.keyCode != "U".charCodeAt(0)) && void 0))
-        )
-} 
+document
+    .addEventListener("contextmenu", ( e => e.preventDefault())), 
+    document
+        .onkeydown = function (e) {
+            return 123 != event.keyCode && (
+                (!e.ctrlKey || !e.shiftKey || e.keyCode != "C".charCodeAt(0)) && ((!e.ctrlKey || !e.shiftKey || e.keyCode != "J".charCodeAt(0)) && ((!e.ctrlKey || e.keyCode != "U".charCodeAt(0)) && void 0))
+                )
+        } 
