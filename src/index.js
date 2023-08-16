3b9fd2006dcd215c066f2fca64b2270f5fcd77b3
anuragjatofficial <anurag.1101140@gmail.com>
2023-08-17 00:24:27 +0530

    fixed disable right click

--- src/index.js
+++ src/index.js
@@ -421,11 +421,11 @@ document
 
 // -- code to disable right click , view source , inspect -- //
 
-// document
-//     .addEventListener("contextmenu", ( e => e.preventDefault())), 
-//     document
-//         .onkeydown = function (e) {
-//             return 123 != event.keyCode && (
-//                 (!e.ctrlKey || !e.shiftKey || e.keyCode != "C".charCodeAt(0)) && ((!e.ctrlKey || !e.shiftKey || e.keyCode != "J".charCodeAt(0)) && ((!e.ctrlKey || e.keyCode != "U".charCodeAt(0)) && void 0))
-//                 )
-//         } 
+document
+    .addEventListener("contextmenu", ( e => e.preventDefault())), 
+    document
+        .onkeydown = function (e) {
+            return 123 != event.keyCode && (
+                (!e.ctrlKey || !e.shiftKey || e.keyCode != "C".charCodeAt(0)) && ((!e.ctrlKey || !e.shiftKey || e.keyCode != "J".charCodeAt(0)) && ((!e.ctrlKey || e.keyCode != "U".charCodeAt(0)) && void 0))
+                )
+        } 
